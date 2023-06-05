259cd8725897ba403ec1ecafda90722fa1767cac
jungyin <noelcool0101@gmail.com>
2023-06-05 21:38:02 +0900

    POJO 상품 등록 기능 구현하기

---
+++ src/test/java/com/example/productorderservice/product/AddProductRequest.java
@@ -0,0 +1,13 @@
+package com.example.productorderservice.product;
+
+import com.example.productorderservice.product.DiscountPolicy;
+import org.springframework.util.Assert;
+
+public record AddProductRequest(String name, int price, DiscountPolicy discountPolicy) {
+    public AddProductRequest {
+        Assert.hasText("상품명은 필수입니다.");
+        Assert.isTrue(price > 0, "상품 가격은 0보다 커야합니다.");
+        Assert.notNull("할인 정책은 필수입니다.");
+    }
+}
+
---
+++ src/test/java/com/example/productorderservice/product/ProductAdapter.java
@@ -0,0 +1,14 @@
+package com.example.productorderservice.product;
+
+public class ProductAdapter implements ProductPort {
+    final ProductRepository productRepository;
+
+    ProductAdapter(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+}
