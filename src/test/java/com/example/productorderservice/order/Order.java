cc60df6df8fd6c1c3f695da1f50d39bf091dcfd0
jungyin <noelcool0101@gmail.com>
2023-06-19 22:42:24 +0900

    JPA 적용하기

--- src/test/java/com/example/productorderservice/order/Order.java
+++ src/test/java/com/example/productorderservice/order/Order.java
@@ -1,20 +1,27 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
-class Order {
-    private Long id;
-    private Product product;
-    private int quantity;
+import javax.persistence.*;
 
-    public Order(Long id, final Product product, final int quantity) {
-        Assert.notNull(product, "상품명은 필수입니다.");
-        Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
-        this.id = id;
-        this.product = product;
-        this.quantity = quantity;
-    }
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Product product;
+
+    private int quantity;
 
     public Order(Product product, int quantity) {
         Assert.notNull(product, "상품명은 필수입니다.");
@@ -23,11 +30,4 @@ class Order {
         this.quantity = quantity;
     }
 
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
